4cb274d4c69e68c2ec1cc23f2495b4f9ae12dc68
lkucera <lkucera@outlook.com>
2017-03-22 17:51:12 +0100

    Update Add-CMUpdateToSUGKB.ps1

--- Add-CMUpdateToSUGKB.ps1
+++ Add-CMUpdateToSUGKB.ps1
@@ -1,11 +1,13 @@
 $updates = gc C:\temp\Update.txt
-
+$SUGName = "TestSUG"
 $allupdates = get-cmsoftwareupdate -fast
 
 Foreach ($Update in $Updates) {
  
- $patches = $allupdates | where {$_.ArticleID -eq $update}
+    $patches = $allupdates | where {$_.ArticleID -eq $update}
  
-    Foreach ($patch in $patches) {Add-CMSoftwareUpdateToGroup -SoftwareUpdateGroupName "Server_MS_Office_updates" -SoftwareUpdateID $patch.CI_ID -verbose}
-  
- }
+    Foreach ($patch in $patches) {
+        Add-CMSoftwareUpdateToGroup -SoftwareUpdateGroupName $SUGName -SoftwareUpdateID $patch.CI_ID -verbose
+    }
+
+}
