896faf11f11d39ecc556559eee0107a5c2d0666c
Lukas <lkucera@outlook.com>
2017-11-03 17:01:18 +0100

    Add files via upload

--- get-cmlogs.ps1
+++ get-cmlogs.ps1
@@ -6,9 +6,11 @@
         [string]$filePath = "C:\windows\ccm\Logs\AlternateHandler.log"
     )
 
-    $file = gc $filePath
-    $obj = @()
+    #load file and initiate variables
 
+    $file = gc $filePath
+    
+    $obj = @()
     $linecount = (Get-Content $filePath | Measure-Object –Line).Lines
     $si=1
     $diverder = [math]::Truncate($linecount/100)
@@ -16,20 +18,23 @@
     $Constructedline = $null
 
 $file | % {
-
+    
+    #display progress of file loading, in order to speed up we dispalay every refresh 1% only
     if( $si/$diverder -eq [math]::Truncate($si/$diverder) ){Write-Progress -Activity "Processing  $si / $linecount" -Status "Loading logfile $filePath" -PercentComplete (($si / $linecount) * 100)}
     
-    #split to content(message) and data
-
+    #check if we are working on constructed line on line from file and proceed acordingly
     if ($Constructedline -eq $null) {$Constructedline = $_}
     else {$Constructedline = $Constructedline + " " + $_}
 
+    #split to content(message) and data
     $CDline = $Constructedline -split("]LOG]!><")
     
+    #check that split was succesfull (if no, it means we work on multiline log entry and we need to contsruct log entry first)
     if($cdline[1] -ne $null)
     {
         $Constructedline = $null
-        #clean the message and data from log remains
+        
+        #tideup the message and data from log remains
         $Message = $CDline[0].replace("<![LOG[","")
         $data = ($CDline[1].replace("<","")).replace(">","")
           
@@ -37,7 +42,6 @@ $file | % {
         $nData = $data.split("="" ")
 
         $loglinefinal = @{
-        
             'Message' = $Message
            # 'Time' = $nData[2]
            # 'Date' = $nData[6]
